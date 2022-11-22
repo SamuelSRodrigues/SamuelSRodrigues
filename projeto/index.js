9fde2ac4df94ae07d4d8d6aa327f5bf72ab3821b
SamuelSRodrigues <116853725+SamuelSRodrigues@users.noreply.github.com>
2022-11-22 07:41:32 -0300

    Blog_Codar

---
+++ projeto/index.js
@@ -0,0 +1,7 @@
+function calculoTriangulo() {
+  const base = prompt("qual valor da base do triangulo?:");
+  const altura = prompt("qual valor da altura do triangulo?;");
+  return base * altura;
+}
+resultado = calculoTriangulo();
+console.log(resultado);
