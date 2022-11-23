9b7d68b547c857c9328be1492287a4e7f22db087
SamuelSRodrigues <116853725+SamuelSRodrigues@users.noreply.github.com>
2022-11-23 07:24:16 -0300

    Delete projeto directory

--- projeto/index.js
+++
@@ -1,7 +0,0 @@
-function calculoTriangulo() {
-  const base = prompt("qual valor da base do triangulo?:");
-  const altura = prompt("qual valor da altura do triangulo?;");
-  return base * altura;
-}
-resultado = calculoTriangulo();
-console.log(resultado);
